4b8617be3e686874cf33c2257dada8417d5e30b2
JE Performance <alencherilalencheril@gmail.com>
2026-08-07 10:29:11 +0530

    slide update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+const slides = document.querySelectorAll(".slide");
 
+let currentSlide = 0;
+
+function showNextSlide() {
+  slides[currentSlide].classList.remove("active");
+
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  slides[currentSlide].classList.add("active");
+}
+
+// Wait 2 seconds before starting
+setTimeout(() => {
+  setInterval(showNextSlide, 10000);
+}, 2000);
